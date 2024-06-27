c538e3dfab3867a178fc65eb66ae10e63b5c31e5
Shadowwing1960 <98994815+Shadowwing1960@users.noreply.github.com>
2024-06-27 16:20:22 +1200

    made a simple title with the url /home and got rid of the the dependency mongodb for now to avoid errors

--- bookhub/bookhub/src/main/java/nicky/tian/bookhub/BookhubApplication.java
+++ bookhub/bookhub/src/main/java/nicky/tian/bookhub/BookhubApplication.java
@@ -2,12 +2,20 @@ package nicky.tian.bookhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BookhubApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookhubApplication.class, args);
+	}
+
+	@GetMapping("/home")
+	public String header() {
+		return "WELCOME TO BOOKHUB!";
 	}
 
 }

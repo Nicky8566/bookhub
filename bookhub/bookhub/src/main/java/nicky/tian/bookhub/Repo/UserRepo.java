a5e7390e312d6e724d537279092045cf35a9bf07
Shadowwing1960 <98994815+Shadowwing1960@users.noreply.github.com>
2024-07-03 17:47:04 +1200

    added reposistory @Repository: Marks the interface as a Spring Data repository  Sorting entity using a id

---
+++ bookhub/bookhub/src/main/java/nicky/tian/bookhub/Repo/UserRepo.java
@@ -0,0 +1,12 @@
+package nicky.tian.bookhub.Repo;
+
+import nicky.tian.bookhub.Entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
+}

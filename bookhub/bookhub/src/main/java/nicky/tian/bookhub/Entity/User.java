886d9609ce93018866b3c833e4c25ce6bde18452
Shadowwing1960 <98994815+Shadowwing1960@users.noreply.github.com>
2024-07-03 16:44:47 +1200

    added the entity which is the user. Also uncommented out Spring Data MongoDB dependcies in the pom.xml file

---
+++ bookhub/bookhub/src/main/java/nicky/tian/bookhub/Entity/User.java
@@ -0,0 +1,55 @@
+package nicky.tian.bookhub.Entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String _id;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String _id, String email, String password) {
+        this._id = _id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "_id='" + _id + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
